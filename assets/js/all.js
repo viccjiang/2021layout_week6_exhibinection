151038369492587db4347ea893f68772d939a5ba
viccjiang <binbinbb520@gmail.com>
2021-08-10 17:19:11 +0800

    Update 2021-08-10T09:19:07.177Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,11 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 //# sourceMappingURL=all.js.map
