da0a260a84da9783d49fe14e89a2e7e935fd3425
viccjiang <binbinbb520@gmail.com>
2021-08-10 17:18:05 +0800

    add swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,10 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
